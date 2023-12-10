e69a4586a8789c9935bcf6e1ac54964e932f1dc0
0Aashish <dellizulter@gmail.com>
2023-12-10 08:21:15 +0545

    see

---
+++ odd-even.js
@@ -0,0 +1,6 @@
+let userInput = prompt("enter a number")
+if(userInput %7==0){
+    alert("multiple of 7");
+}else {
+    alert("not multiple")
+}
---
+++ seeGrade.js
@@ -0,0 +1,26 @@
+let userScore=prompt("inputc your grade point ")
+
+if (userScore >= 90)
+{
+
+
+  alert(  " garde = A+");
+}else if (userScore >= 80){
+    alert("garde = A")
+
+}else if (userScore >=70){
+    alert("grade=B+")
+}
+else if (userScore >=60){
+alert("grade=B")
+}
+else if (userScore >=50){
+    alert("grade=C+")
+}
+else if (userScore >=40){
+    alert("grade=C")
+}
+
+else {
+    alert("grade =  fail")
+}
