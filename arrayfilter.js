3f3df46d52de6649a89304ef871ff3e4a5044c62
0Aashish <dellizulter@gmail.com>
2024-01-08 07:57:21 +0545

    exp 12

---
+++ arrayfilter.js
@@ -0,0 +1,5 @@
+let stringsArray = ["Apple", 'Mango', 'Papaya', 'Kiwi', 'pineapple','watermelon','Avarcado'];
+
+let filteredArray = stringsArray.filter(str => str.startsWith('A'));
+
+console.log("Filtered strings starting with 'A':", filteredArray);
---
+++ arraysort.js
@@ -0,0 +1,7 @@
+
+const stringsArray = ['Mango', 'Papaya', 'Kiwi', 'pineapple','watermelon'];
+
+stringsArray.sort();
+
+console.log("Sorted array of strings:");
+console.log(stringsArray);
